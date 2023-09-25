41f273be823c915a29d856c02d0068b89680d2b4
Lucas Teixeira Soares <84486266+lucastso10@users.noreply.github.com>
2023-09-25 19:04:57 -0300

    Add files via upload

---
+++ GrafoMatriz.py
@@ -0,0 +1,260 @@
+from tabulate import tabulate
+
+class GrafoMatriz:
+  TAM_MAX_DEFAULT = 100 # qtde de vértices máxima default
+  # construtor da classe grafo
+  def __init__(self, n=TAM_MAX_DEFAULT):
+    self.n = n # número de vértices
+    self.m = 0 # número de arestas
+    # matriz de adjacência
+    self.nomes = []
+    self.adj = [[0 for i in range(n)] for j in range(n)]
+  
+  # Insere uma aresta no Grafo tal que
+  # v é adjacente a w
+  def insereA(self, v, w, val):
+    if self.adj[v][w] == 0:
+      self.adj[v][w] = val
+      self.m+=1 # atualiza qtd arestas
+  
+  # remove uma aresta v->w do Grafo	
+  def removeA(self, v, w):
+    # testa se temos a aresta
+    if self.adj[v][w] >= 1:
+      self.adj[v][w] = 0
+      self.m-=1; # atualiza qtd arestas
+
+  def insereV(self, val):
+    self.n += 1
+    novoV = [0 for i in range(self.n)]
+    self.adj.append(novoV)
+    for i in range(self.n-1):
+      self.adj[i].append(0)
+
+    self.nomes.append(val)
+    
+  
+  def removeV(self, v):
+    adjNovo = [[0 for i in range(self.n - 1)] for j in range(self.n - 1)]
+
+    for i in range(self.n):
+      self.removeA(i, v)
+      self.removeA(v, i)
+  
+    for i in range(self.n - 1):
+      for j in range(self.n - 1):
+        if i >= v and j >= v:
+          adjNovo[i][j] = self.adj[i+1][j+1]
+        elif i >= v and j < v:
+          adjNovo[i][j] = self.adj[i+1][j]
+        elif i < v and j >= v:
+          adjNovo[i][j] = self.adj[i][j+1]
+        else:
+          adjNovo[i][j] = self.adj[i][j]
+
+    self.n -= 1
+    
+    self.adj = adjNovo
+
+    self.nomes.pop(v)
+  
+  # Apresenta o Grafo contendo
+  # número de vértices, arestas
+  # e a matriz de adjacência obtida	
+  def show(self):
+    print(f"\n n: {self.n:2d} ", end="")
+    print(f"m: {self.m:2d}\n")
+
+    temp_adj = []
+
+    for i in range(4, self.n, 4):
+      for j in range(self.n):
+        if i < self.n:
+          line = self.adj[j][i-4:i]
+          line.insert(0, self.nomes[j] + f"({j})")
+          temp_adj.append(line)
+        else:
+          line = self.adj[j][i-4:self.n]
+          line.insert(0, self.nomes[j] + f"({j})")
+          temp_adj.append(line)
+
+      nomes = self.nomes[i-4:i]
+      nomes.insert(0, "")
+      print(tabulate(temp_adj, nomes, tablefmt="pretty"))
+
+      temp_adj = []
+
+    if self.n % 4 != 0:
+      for j in range(self.n):
+        line = self.adj[j][self.n - (self.n % 4):self.n]
+        line.insert(0, self.nomes[j] + f"({j})")
+        temp_adj.append(line)
+
+      nomes = self.nomes[self.n - (self.n % 4):self.n]
+      nomes.insert(0, "")
+      print(tabulate(temp_adj, nomes, tablefmt="pretty"))
+  
+  # Apresenta o Grafo contendo
+  # número de vértices, arestas
+  # e a matriz de adjacência obtida 
+  # Apresentando apenas os valores 0 ou 1	
+  def showMin(self):
+    print(f"\n n: {self.n:2d} ", end="")
+    print(f"m: {self.m:2d}\n")
+    for i in range(self.n):
+      for w in range(self.n):
+        if self.adj[i][w] >= 1:
+          print(f" {self.adj[i][w]} ", end="") 
+        else:
+          print(" 0 ", end="")
+      print("\n")
+    print("\nfim da impressao do grafo." )
+
+  
+  def inDegree(self, v):
+    degree = 0
+    for i in range(self.n):
+      if self.adj[i][v] == 1:
+        degree += 1
+
+    return degree
+  
+  def outDegree(self, v):
+    degree = 0
+    for i in range(self.n):
+      if self.adj[v][i] == 1:
+        degree += 1
+  
+    return degree  
+  
+  def ehFonte(self, v):
+    if self.inDegree(v) == 0 and self.outDegree(v) > 0:
+      return 1
+    return 0
+  
+  def ehSorvedouro(self, v):
+    if self.inDegree(v) > 0 and self.outDegree(v) == 0:
+      return 1
+    return 0
+  
+  def ehSimetrico(self):
+    if self.m % 2  != 0:
+      return 0
+  
+    for i in range(self.n):
+      for j in range(i, self.n):
+        if self.adj[i][j] == 1 and self.adj[j][i] == 0:
+          return 0
+      
+    return 1
+
+  def ehCompleto(self):
+    for i in range(self.n):
+      for j in range(self.n):
+        if self.adj[i][j] == 0 and i != j:
+          return 0
+    return 1
+
+
+  def caminhoPossivelIntern(self, x, y, rota):
+    if x == y:
+      return True
+      
+    rota.add(x)
+
+    for i in range(self.n):
+      if self.adj[x][i] == 1 and i not in rota and self.caminhoPossivelIntern(i, y, rota):
+        return True
+
+    return False
+  
+  # se é possivel chegar no vertice y a partir do vertice x  
+  def caminhoPossivel(self, x, y):
+    rota = set()
+    return self.caminhoPossivelIntern(x, y, rota)
+    
+
+  def categoriaConexidade(self):
+    conectados = {0}
+    novos_conectados = {0}
+    antigo = 0
+    
+    while antigo < len(conectados):
+      antigo = len(conectados)
+
+      for x in conectados:
+        for i in range(self.n):
+          if self.adj[x][i] == 1 or self.adj[i][x] == 1:
+            novos_conectados.add(i)
+
+      conectados = conectados.union(novos_conectados)
+      novos_conectados.clear()
+
+    if self.n != len(conectados):
+      return 0
+
+    for i in range(self.n):
+      for j in range(i, self.n):
+        if not self.caminhoPossivel(i, j) and not self.caminhoPossivel(j, i):
+          return 1
+
+    for i in range(self.n):
+      for j in range(self.n):
+        if not (self.caminhoPossivel(i, j) and self.caminhoPossivel(i,j)):
+          return 2
+
+    return 3
+
+
+  def grafoReduzido(self):
+    RMais = {0}
+    RMenos = {0}
+    Total = {0}
+    S = []
+    verticeAtual = 0
+
+    while len(Total) != self.n:
+
+      RMais.add(verticeAtual)
+      RMenos.add(verticeAtual)
+
+      # calcula o RMais desse vertice
+      for i in range(self.n):
+        if i in Total:
+          continue
+        if self.caminhoPossivel(verticeAtual, i):
+          RMais.add(i)
+
+      # calcula o RMenos desse vertice
+      for i in range(self.n):
+        if i in Total:
+          continue
+        
+        if self.caminhoPossivel(i, verticeAtual):
+          RMenos.add(i)
+
+      # pega a intersecção e coloca na lista
+      S.append(list(RMais.intersection(RMenos)))
+      Total = Total.union(RMais.intersection(RMenos))
+
+      RMais.clear()
+      RMenos.clear()
+
+      # pega o maior vertice de todas as intersecções e soma um para
+      # decidir o proximo vertice que vai calcular o RMais e RMenos
+      verticeAtual = max(Total) + 1
+
+    # cria um grafo com a quantidade de secções do outro
+    Gr = GrafoMatriz(len(S))
+
+    # for bem feio para calcular todas as arestas do grafo reduzido
+    # ela compara cada vertice de cada secção com cada vertice das outras
+    # secções pra determinar se tem uma aresta entre os vertices do novo grafo
+    for i in range(len(S)):
+      for j in range(len(S[i])):
+        for k in range(len(S)):
+          for l in range(len(S[k])):
+            if i != k and self.caminhoPossivel(S[i][j], S[k][l]):
+              Gr.insereA(i,k)
+
+    return Gr
---
+++ main.py
@@ -0,0 +1,229 @@
+import pandas as pd
+import openpyxl
+from GrafoMatriz import GrafoMatriz
+
+def print_menu():
+  print("╔═══════════════════════════════════════════╗")
+  print("║                 NaveGrafos                ║")
+  print("║            Selecione uma opção!           ║")
+  print("╠═══════════════════════════════════════════╣")
+  print("║ 1.Ler arquivo                             ║")
+  print("║                                           ║")
+  print("║ 2.Salvar dados                            ║")
+  print("║                                           ║")
+  print("║ 3.Inserir vértice                         ║")
+  print("║                                           ║")
+  print("║ 4.Inserir aresta                          ║")
+  print("║                                           ║")
+  print("║ 5.Remover vértice                         ║")
+  print("║                                           ║")
+  print("║ 6.Remover aresta                          ║")
+  print("║                                           ║")
+  print("║ 7.Conteúdo do arquivo                     ║")
+  print("║                                           ║")
+  print("║ 8.Mostrar grafo                           ║")
+  print("║                                           ║")
+  print("║ 9.Conexidade                              ║")
+  print("║                                           ║")
+  print("║ 0.Sair                                    ║")
+  print("╚═══════════════════════════════════════════╝")
+
+def escolherOpcao():
+  valido = False
+
+  while not valido:
+    x = input()
+    try:
+      x = int(x)
+    except ValueError:
+      print("Porfavor digite o número de uma das opções!")
+      continue
+    if x > 9 or x < 0:
+      print("Porfavor digite o número de uma das opções!")
+      continue
+    valido = True
+
+  return x
+
+def lerArquivo():
+  with open("grafo.txt") as f:
+    line = f.readline()
+    line = f.readline()
+    g = GrafoMatriz(int(line))
+
+    for i in range(int(line)):
+      line = f.readline()
+      temp = line.split(" ")
+      g.nomes.append(temp[1].replace('\n', ''))
+      
+    
+    line = f.readline()
+    
+    for i in range(int(line)):
+        line = f.readline()
+        aresta = line.split(" ")
+        g.insereA(int(aresta[0]), int(aresta[1]), int(aresta[2]))
+
+  return g
+
+def salvarDados(g):
+  with open("grafo.txt", "w") as f:
+    f.write("2\n")
+    f.write(f"{g.n}\n")
+    for i in range(g.n):
+      f.write(f"{i} {g.nomes[i]}\n")
+    f.write(f"{g.m}\n")
+    for i in range(g.n):
+      for j in range(g.n):
+        if g.adj[i][j] >= 1:
+          f.write(f"{i} {j} {g.adj[i][j]}\n")
+
+def printArquivo():
+  with open("grafo.txt") as f:
+    line = f.readline()
+    tipo = int(line)
+    match tipo:
+      case 0:
+        print("\nO grafo é não orientado e sem peso")
+      case 1:
+        print("\nO grafo é não orientado com peso no vértice")
+      case 2:
+        print("\nO grafo é não orientado com peso nas arestas")
+      case 3:
+        print("\nO grafo é não orientado com peso nos vértices e arestas")
+      case 4:
+        print("\nO grafo é orientado sem peso")
+      case 5:
+        print("\nO grafo é orientado com peso no vértice")
+      case 6:
+        print("\nO grafo é orientado com peso na aresta")
+      case 7:
+        print("\nO grafo é orientado com peso nos vértices e arestas")
+
+    line = f.readline()
+    print(f"\nele possui {line[:-1]} vértices:")
+
+    for i in range(int(line)):
+      line = f.readline()
+      line = line.split(" ")
+      if tipo == 1 or tipo == 3 or tipo == 5 or tipo == 7:
+        print(f"número:{line[0]}, nome: {line[1]}, peso: {line[2]}")
+      else:
+        print(f"número:{line[0]}, nome: {line[1]}")
+
+    line = f.readline()
+    print(f"\nele possui {line[:-1]} arestas:")
+
+    for i in range(int(line)):
+      line = f.readline()
+      line = line.split(" ")
+      if tipo == 2 or tipo == 3 or tipo == 6 or tipo == 7:
+        print(f"de:{line[0]}, para: {line[1]}, peso: {line[2]}")
+      else:
+        print(f"de:{line[0]}, para: {line[1]}")
+
+
+
+def main():
+  print("Bem Vindo!")
+
+  rodar = True
+
+  while rodar:
+    print_menu()
+    x = escolherOpcao()
+
+    match x:
+      case 1:
+        # ler arquivo
+        g = lerArquivo()
+        print("\nO arquivo grafo.txt foi lido com êxito!\n")
+        
+      case 2:
+        # salvar dados
+        if g:
+          salvarDados(g)
+          print("\nDados salvos com êxito")
+        else:
+          print("\nNão existe nenhum grafo!\n")
+
+      case 3:
+        # inserir vertice
+        if g:
+          print("\nDigite o nome do novo vértice!")
+          nome = input()
+          g.insereV(nome)
+          print("\nNome vértice inserido com sucesso!\n")
+        else:
+          print("\nNão existe nenhum grafo!\n")
+
+      case 4:
+        # inserir aresta
+        if g:
+          print("\nQual vértice gostaria de conectar? (digite o número dele)\n")
+          i = input()
+          print(f"\nCom qual vértice gostaria de conectar o vértice {i}? (digite o número dele)\n")
+          j = input()
+          print("\nQual seria o valor desse vértice?")
+          val = input()
+          g.insereA(int(i), int(j), int(val))
+          g.insereA(int(j), int(i), int(val))
+          print("\nNova aresta inserida com sucesso!\n")
+        else:
+          print("\nNão existe nenhum grafo!\n")
+
+      case 5:
+        # remover vertice
+        if g:
+          print("\nQual vértice gostaria de remover? (digite o número dele)\n")
+          i = input()
+          g.removeV(int(i))
+          print("\nVértice removido com sucesso!\n")
+        else:
+          print("\nNão existe nenhum grafo!\n")
+
+      case 6:
+        # remover aresta
+        if g:
+          print("\nDe qual vértice começa a aresta? (digite o número dele)\n")
+          i = input()
+          print("\nDe qual vértice começa a aresta? (digite o número dele)\n")
+          j = input()
+          g.removeA(int(i), int(j))
+          g.removeA(int(j), int(i))
+          print("\nAresta removida com sucesso!\n")
+        else:
+          print("\nNão existe nenhum grafo!\n")
+
+      case 7:
+        # conteudo do arquivo
+        printArquivo()
+
+      case 8:
+        # mostrar grafo
+        if g:
+          g.show()
+        else:
+          print("\nNão existe nenhum grafo!\n")
+
+      case 9:
+        #conexidade
+        if g:
+          if g.categoriaConexidade() > 0:
+            print("\nO grafo não é conexo!\n")
+          else:
+            print("\nO grafo é conexo!\n")
+        else:
+          print("\nNão existe nenhum grafo!\n")
+
+      case 0:
+        #sair
+        rodar = False
+
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
